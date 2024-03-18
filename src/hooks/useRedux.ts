2f32a68ba7ad5b3c1ff08392dbf2c29cb39f7183
ObuchiYuki <yukibochi1@gmail.com>
2024-03-18 11:35:17 +0900

    sync

--- src/hooks/useRedux.ts
+++ src/hooks/useRedux.ts
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import type { AppDispatch } from '../redux/store'
-import { MarkNoteDocument } from '../redux/documentSlice'
+import type { MarkNoteDocument } from '../model/MarkNoteDocument'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
